9b29b9affef2cff4d0f7232b3334bb9c0c39b88a
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2016-10-14 13:14:36 +0000

    Add gems and Posts scaffolding

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,14 +43,14 @@ Rails.application.configure do
   config.assets.debug = true
 
   # Suppress logger output for asset requests.
-  config.assets.quiet = true
+  config.assets.quiet = false
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
